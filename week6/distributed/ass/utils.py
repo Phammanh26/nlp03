798dca5db81faa1a8049a983abbae0ee908edb01
pvm <manh2604.pham@gmail.com>
2023-05-17 11:42:09 +0700

    update homeworks

--- week6/distributed/ass/utils.py
+++ week6/distributed/ass/utils.py
@@ -35,4 +35,5 @@ class Prompter(object):
     
 
 def download_from_driver(data_driver_path, location_path):
+    print(f"begin download....: {data_driver_path}")
     gdown.download(data_driver_path, location_path, quiet=False, fuzzy=True)
