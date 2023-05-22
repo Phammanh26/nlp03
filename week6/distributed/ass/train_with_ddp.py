a75476927d87d48b92f3a09bbeb2161eb2195839
pphuc25 <phanphuc1100@gmail.com>
2023-05-22 00:08:28 +0000

    bs 4

--- week6/distributed/ass/train_with_ddp.py
+++ week6/distributed/ass/train_with_ddp.py
@@ -263,7 +263,7 @@ if __name__ == "__main__":
     size_valid_set = 0.1
     max_length = 512
     num_epochs = 3
-    batch_size = 8
+    batch_size = 4
 
     learning_rate = 1e-5
     lr_scheduler_type = 'cosine'
