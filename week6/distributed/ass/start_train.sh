2aafcbcfabdc5bccb26b54192292242502b26013
pvm <manh2604.pham@gmail.com>
2023-05-23 10:03:14 +0700

    update script training

--- week6/distributed/ass/start_train.sh
+++ week6/distributed/ass/start_train.sh
@@ -1,5 +1,5 @@
 export DEBUG=0
 git clone https://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@github.com/Phammanh26/nlp03.git
-pip install -r  nlp03/week6/distributed/ass/requirements.txt
-cd nlp03/week6/distributed/ass"
+pip install -r  $pwd/nlp03/week6/distributed/ass/requirements.txt
+cd $pwd/nlp03/week6/distributed/ass"
 torchrun --nproc_per_node=2 --master_port=1234 train_with_ddp_solution.py
