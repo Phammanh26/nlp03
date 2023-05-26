0281aa3a6956927efae67fa1379d027b267723ef
pvm <manh2604.pham@gmail.com>
2023-05-27 06:59:21 +0700

    add module generate

--- week6/distributed/ass/inference.py
+++ week6/distributed/ass/inference.py
@@ -17,63 +17,64 @@ def get_response(prompt, tokenizer, model, generation_config, max_new_tokens):
     output = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
     return output
 
-def generate_inference(instruction: str, user_inp: str, model_path:str, lora_weights_path:str):
-    
-    top_k=40
-    top_p=128
-    temperature=0.1
-    load_8bit=True
-    num_beams =1
-    max_new_tokens =128
 
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    config = AutoConfig.from_pretrained(model_path)
-    architecture = config.architectures[0]
-    if "Llama" in architecture:
-        print("Setting EOS, BOS, UNK, and PAD tokens for LLama tokenizer")
-        tokenizer.add_special_tokens(
-            {
-                "eos_token": "</s>",
-                "bos_token": "</s>",
-                "unk_token": "</s>",
-            }
-        )
-        tokenizer.pad_token_id = (
-            0  # unk. we want this to be different from the eos token
-        )
-        tokenizer.padding_side = "left"
-    
-    model = AutoModelForCausalLM.from_pretrained(
-            model_path,
-            load_in_8bit=load_8bit,
-            torch_dtype=torch.float16,
-            device_map="auto")
-    model = LoraModelForCasualLM.from_pretrained(
-            model,
-            lora_weights_path,
-            torch_dtype=torch.float16)
+
+class Generater:
+    def __init__(self, model_path, lora_weights_path, load_8bit, max_new_tokens: int =128):
+        self.max_new_tokens =max_new_tokens 
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        config = AutoConfig.from_pretrained(model_path)
+        architecture = config.architectures[0]
+        if "Llama" in architecture:
+            print("Setting EOS, BOS, UNK, and PAD tokens for LLama tokenizer")
+            tokenizer.add_special_tokens(
+                {
+                    "eos_token": "</s>",
+                    "bos_token": "</s>",
+                    "unk_token": "</s>",
+                }
+            )
+            tokenizer.pad_token_id = (
+                0  # unk. we want this to be different from the eos token
+            )
+            tokenizer.padding_side = "left"
         
-    model.config.pad_token_id = tokenizer.pad_token_id
-    model.config.bos_token_id = tokenizer.bos_token_id
-    model.config.eos_token_id = tokenizer.eos_token_id
-    model.eval()
+        model = AutoModelForCausalLM.from_pretrained(
+                model_path,
+                load_in_8bit=load_8bit,
+                torch_dtype=torch.float16,
+                device_map="auto")
+        self.model = LoraModelForCasualLM.from_pretrained(
+                model,
+                lora_weights_path,
+                torch_dtype=torch.float16)
+        
+        self.model.config.pad_token_id = tokenizer.pad_token_id
+        self.model.config.bos_token_id = tokenizer.bos_token_id
+        self.model.config.eos_token_id = tokenizer.eos_token_id
+        self.model.eval()
+        
+        self.prompter = Prompter()
+
+    def generate(self, instruction: str, user_inp: str, temperature: float):
+
+        top_k=40
+        top_p=128
+        temperature=0.1
+        num_beams =1
     
-    generation_config = GenerationConfig(
-            temperature=temperature,
-            top_p=top_p,
-            top_k=top_k,
-            num_beams=num_beams)
-    
-    prompter = Prompter()
-    
-   
-    
-    # instruction = input("Your Instruction: ")
-    # user_inp = input("Your input (enter n/a if there is no): ")
-    if user_inp.lower().strip() == "n/a":
-        user_inp = None
-    prompt = prompter.generate_prompt(instruction, user_inp)
-    output = get_response(prompt, tokenizer, model, generation_config, max_new_tokens)
-    response = prompter.get_response(output)
-    return response
-    
+        self.generation_config = GenerationConfig(
+                temperature=temperature,
+                top_p=top_p,
+                top_k=top_k,
+                num_beams=num_beams)
+
+        # instruction = input("Your Instruction: ")
+        # user_inp = input("Your input (enter n/a if there is no): ")
+        if user_inp.lower().strip() == "n/a":
+            user_inp = None
+        prompt = self.prompter.generate_prompt(instruction, user_inp)
+        output = get_response(prompt, self.model, self.generation_config, self.max_new_tokens)
+        response = self.prompter.get_response(output)
+        return response
+        
