693726150b02d5bcc8bc0884b96d8455d4352501
pvm <manh2604.pham@gmail.com>
2023-05-27 07:06:39 +0700

    add module generate

--- week6/distributed/ass/inference.py
+++ week6/distributed/ass/inference.py
@@ -22,22 +22,22 @@ def get_response(prompt, tokenizer, model, generation_config, max_new_tokens):
 class Generater:
     def __init__(self, model_path, lora_weights_path, load_8bit, max_new_tokens: int =128):
         self.max_new_tokens =max_new_tokens 
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
         config = AutoConfig.from_pretrained(model_path)
         architecture = config.architectures[0]
         if "Llama" in architecture:
             print("Setting EOS, BOS, UNK, and PAD tokens for LLama tokenizer")
-            tokenizer.add_special_tokens(
+            self.tokenizer.add_special_tokens(
                 {
                     "eos_token": "</s>",
                     "bos_token": "</s>",
                     "unk_token": "</s>",
                 }
             )
-            tokenizer.pad_token_id = (
+            self.tokenizer.pad_token_id = (
                 0  # unk. we want this to be different from the eos token
             )
-            tokenizer.padding_side = "left"
+            self.tokenizer.padding_side = "left"
         
         model = AutoModelForCausalLM.from_pretrained(
                 model_path,
@@ -49,9 +49,9 @@ class Generater:
                 lora_weights_path,
                 torch_dtype=torch.float16)
         
-        self.model.config.pad_token_id = tokenizer.pad_token_id
-        self.model.config.bos_token_id = tokenizer.bos_token_id
-        self.model.config.eos_token_id = tokenizer.eos_token_id
+        self.model.config.pad_token_id = self.tokenizer.pad_token_id
+        self.model.config.bos_token_id = self.tokenizer.bos_token_id
+        self.model.config.eos_token_id = self.tokenizer.eos_token_id
         self.model.eval()
         
         self.prompter = Prompter()
@@ -74,7 +74,7 @@ class Generater:
         if user_inp.lower().strip() == "n/a":
             user_inp = None
         prompt = self.prompter.generate_prompt(instruction, user_inp)
-        output = get_response(prompt, self.model, self.generation_config, self.max_new_tokens)
+        output = get_response(prompt, self.tokenizer, self.model, self.generation_config, self.max_new_tokens)
         response = self.prompter.get_response(output)
         return response
         
